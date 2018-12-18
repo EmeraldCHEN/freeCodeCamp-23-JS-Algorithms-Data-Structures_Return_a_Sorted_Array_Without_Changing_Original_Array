b2c789cb642952fbdc8f5e6095f23755c17e8826
EmeraldNZ <ahyuancy@gmail.com>
2018-12-19 09:40:49 +1300

    Update .js

--- .js
+++ .js
@@ -2,7 +2,7 @@ var globalArray = [0, 3, 2, 1];
 
 function nonMutatingSort(arr) { 
 
-    return arr.slice(0).sort((a,b) =>a - b); // Use slice() to duplicate arr firstly
+    return arr.slice().sort((a,b) =>a - b); // Use slice() to duplicate arr firstly
     
 }
 console.log(nonMutatingSort(globalArray)); // [2, 3, 5, 6, 9]
