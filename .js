a90f7d2bc4107677f9c478999fc32cd70b2a37cf
EmeraldNZ <ahyuancy@gmail.com>
2018-12-19 09:37:58 +1300

    Update .js

--- .js
+++ .js
@@ -2,16 +2,17 @@ var globalArray = [0, 3, 2, 1];
 
 function nonMutatingSort(arr) { 
 
-    return arr.slice(0).sort((a,b) =>a - b);
+    return arr.slice(0).sort((a,b) =>a - b); // Use slice() to duplicate arr firstly
     
 }
-console.log(nonMutatingSort(globalArray));
-console.log(globalArray);
+console.log(nonMutatingSort(globalArray)); // [2, 3, 5, 6, 9]
+console.log(globalArray); // [5, 6, 3, 2, 9]
 
+///////////////////////////////////////////////////////////////////////////////////////////////
 
-function nonMutatingSort(arr) {
+function nonMutatingSorting(arr) {
 
      let newArr = [];
-     return newArr.concat(arr).sort((a,b) => a-b);
+     return newArr.concat(arr).sort((a,b) => a-b); // Concatenate an empty array to the one being sorted as concat returns a new array
      
 }
